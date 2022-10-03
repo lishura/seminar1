4bd657be99a50ccb8c5548bade1d9e8059c2f318
Alexandra Lisovskaya <lishura@rambler.ru>
2022-10-03 20:56:42 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 7;
+int b = 4;
+
+if(a>b)
+{
+    Console.Write("Максимальное значение ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("Максимальное значение ");
+    Console.WriteLine(b);
+}
