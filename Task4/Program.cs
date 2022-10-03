3de14bc801e83434269caca4ac5f77de7bf2f9f0
Alexandra Lisovskaya <lishura@rambler.ru>
2022-10-03 21:15:55 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = 48888;
+int b = 5699999;
+int c = 643;
+
+int max = a;
+
+
+if (b > a)
+{
+    max = b;
+
+    if (c > b)
+    {
+        max = c;
+    }
+
+}
+
+Console.Write("Максимальное число ");
+Console.WriteLine(max);
