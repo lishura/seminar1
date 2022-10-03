9d4f0bd570b0e9f05b6a0a44c1322d3a68c35400
Alexandra Lisovskaya <lishura@rambler.ru>
2022-10-03 22:46:55 +0300

    add Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число больше 1: ");
+string value = Console.ReadLine();
+int n = int.Parse(value);
+
+Console.WriteLine();
+
+if (n <= 1)
+{
+    Console.WriteLine("Неверное число. Вы ввели число меньше 1");
+}
+else
+{
+    int count = 2;
+    while (count < (n + 1))
+    {
+        Console.Write(count);
+        Console.Write(" ");
+        count = count + 2;
+    }
+}
