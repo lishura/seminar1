04ef53afd59f8d1a15ef848e6a17531e3a8d470d
Alexandra Lisovskaya <lishura@rambler.ru>
2022-10-03 21:59:32 +0300

    add Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+string value = Console.ReadLine();
+int x = int.Parse(value);
+
+if(x%2==0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
